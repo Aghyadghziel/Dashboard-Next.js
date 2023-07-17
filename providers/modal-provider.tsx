30b0a9719a5e7fa0ef705a27758df959796a6e4a
aghyadghziel <EDGXaghyad@gmail.com>
2023-07-18 02:34:00 +0300

    fir

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -1,3 +1,21 @@
-"use client"
+"use client";
 
-impo
+import { StoreModal } from "@/components/modals/store-modal";
+import { useEffect, useState } from "react";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <StoreModal />
+    </>
+  );
+};
