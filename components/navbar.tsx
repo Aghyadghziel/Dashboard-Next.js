49ebe28aaf9e6e067a3c8e2817d3dfb8f0c26e0b
aghyadghziel <EDGXaghyad@gmail.com>
2023-07-25 14:52:27 +0300

    s

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
-import { MainNav } from "./main-nav";
+import { MainNav } from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
 
 const Navbar = () => {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4 ">
-        <div>this is the left</div>
+        <StoreSwitcher />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
